ab84a36861ea26f9e14778a2c4a8510b3603dcdc
zhaojian <flannery.zhao@kikatech.com>
2023-08-28 12:06:53 +0800

    Kotlin 输出

---
+++ app/src/main/java/com/flannery/kotlinsmali/Test7.kt
@@ -0,0 +1,15 @@
+package com.flannery.kotlinsmali
+
+class Test7 {
+
+    fun main(args : Array<String>) {
+        val score = 12.3
+
+        println("score")
+        println("$score")
+        println("score = $score")
+        println("${score + score}")
+        println(12.3)
+    }
+
+}
